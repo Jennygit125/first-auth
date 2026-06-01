198ffb4238684f47fb09712f23055fef33f49b97
Jennygit125 <159704356+Jennygit125@users.noreply.github.com>
2026-06-01 09:05:45 +0100

    built for vercel

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // here lies information for the computer/server noticed all dependencies except mongoose called here seems like a coincidence there should be other function specific dependencies like mongoose
 
-const express = require ("express")
+const express = require ("express");
+const cors = require ("cors");//planning to link this to an existing frontend
 const morgan = require("morgan");
 require ("dotenv").config();
 const connectDb = require("./src/config/db");
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true // This is required if you are sending cookies or authorization headers
+}));
 const userRoutes = require("./src/routes/routes.js")
 
 const port = process.env.PORT|| 4000;
@@ -21,7 +26,13 @@ app.get("/", (req,res) =>{
 
 
 // funny improvement on my side this connects to db before running on port might be an inefficent format but i don't wanna use if or something like that here
-connectDb().then(()=>{app.listen(port, () =>{
-    console.log("server is running for real");
-});
-});
+connectDb().then
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Server running locally on port ${PORT}`);
+  });
+}
+// i like how fast vercel can be but this thing causes issues sometimes
+// Export the app for Vercel's serverless environment
+module.exports = app;
