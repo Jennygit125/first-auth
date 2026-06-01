aa85b25f3202418083e58ec8fd0a36e0a1128294
Jennygit125 <159704356+Jennygit125@users.noreply.github.com>
2026-06-01 20:59:47 +0100

    added role to response

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -151,7 +151,8 @@ const signIn = async (req, res) => {
         id: user._id,
         firstName: user.firstName,
         lastName: user.lastName,
-        email: user.email
+        email: user.email,
+        role: user.role
         }, token // returns everything except password or hashed password no where in my code would you find the password it lives and stays in the database
 
     });
